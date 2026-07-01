f6fa99b5d95d250bb4edbefecadd8ae2a972f8cc
bobtrrr <bob_trrr@mail.ru>
2026-07-01 15:19:32 +0700

    Рефакторинг

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,11 @@
 /* в этот файл добавляет скрипты*/
-import {initToggleMenu} from './nav';
+import {initToggleMenu, initCangeSiteNavTheme} from './nav';
 import { initResponsiveButton } from './responsive-button';
 
 const init = () => {
   initToggleMenu();
   initResponsiveButton();
+  initCangeSiteNavTheme();
 };
 
 init();
--- source/scripts/nav.js
+++ source/scripts/nav.js
@@ -10,3 +10,35 @@ const toggleMenu = () => {
 };
 
 export const initToggleMenu = () => toggleMenu();
+
+
+const DESKTOP_WIDTH = 1260;
+const PAGE_TYPE = 'index';
+
+const pageType = document.documentElement.dataset.pageType;
+const mediaQuery = window.matchMedia(`(min-width: ${DESKTOP_WIDTH}px)  `);
+
+const handleScreenChange = (evt) => {
+  if (pageType === PAGE_TYPE) {
+    if (evt.matches) {
+      navList.classList.add('site-nav--theme-accent');
+
+      return;
+    }
+
+    navList.classList.remove('site-nav--theme-accent');
+  }
+
+};
+
+const changeSiteNavTheme = () => {
+  if (!navList) {
+    return;
+  }
+
+  mediaQuery.addEventListener('change', handleScreenChange);
+  handleScreenChange(mediaQuery);
+};
+
+export const initCangeSiteNavTheme = () => changeSiteNavTheme();
+
