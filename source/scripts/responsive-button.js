3bd6ab1cfba55823a6f419d57d935d63afe32ec2
bobtrrr <bob_trrr@mail.ru>
2026-07-03 05:27:52 +0700

    Добавит form.html

--- source/scripts/responsive-button.js
+++ source/scripts/responsive-button.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('.hero__button');
+const formButton = document.querySelector('.checkout-form__button');
 
 const TABLET_WIDTH = 748;
 
@@ -6,23 +7,39 @@ const mediaQuery = window.matchMedia(`(min-width: ${TABLET_WIDTH}px)  `);
 
 const handleScreenChange = (evt) => {
   if (evt.matches) {
-    button.classList.remove('button--size-m');
-    button.classList.add('button--size-l');
+    if (button) {
+      button.classList.remove('button--size-m');
+      button.classList.add('button--size-l');
+    }
 
+    if (formButton) {
+      formButton.classList.remove('button--size-m');
+      formButton.classList.add('button--size-l');
+    }
     return;
   }
 
-  button.classList.remove('button--size-l');
-  button.classList.add('button--size-m');
+  if (button) {
+    button.classList.remove('button--size-l');
+    button.classList.add('button--size-m');
+  }
+
+  if (formButton) {
+    formButton.classList.remove('button--size-l');
+    formButton.classList.add('button--size-m');
+  }
 };
 
 const responsiveButton = () => {
-  if (!button) {
-    return;
+  if (button) {
+    mediaQuery.addEventListener('change', handleScreenChange);
+    handleScreenChange(mediaQuery);
   }
 
-  mediaQuery.addEventListener('change', handleScreenChange);
-  handleScreenChange(mediaQuery);
+  if (formButton) {
+    mediaQuery.addEventListener('change', handleScreenChange);
+    handleScreenChange(mediaQuery);
+  }
 };
 
 export const initResponsiveButton = () => responsiveButton();
