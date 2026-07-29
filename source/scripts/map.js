fcb66ea5747ad52976eb32b214ebc2c2b733127d
bob-ТРАКТОРИСТ! <108051824+bobtrrr@users.noreply.github.com>
2026-07-30 06:07:27 +0700

    Module5 task1 (#6)
    * fix fonts.scss
    
    * fix readme
    
    * Добавит секцию diet
    
    * Добавит features
    
    * Добавит секцию result
    
    * Добавит секцию Contacts
    
    * Добавит секцию page-footer
    
    * Стилизует блок с картой
    
    * Добавит header и hero
    
    * Рефакторинг Catalog
    
    * Добавит catalog
    
    * Добавит additional-products
    
    * fix секцию gift
    
    * Добавит секцию newsletter и рефакторинг др. блоков
    
    * Рефакторинг
    
    * Добавит form.html
    
    * Добавит slider
    
    * добавит кашу
    
    * Рефакторин page.scss
    
    * Рефакторинг slider
    
    * Добавит title
    
    * Рефакторинг footer
    
    * Рефакторинг page-footer
    
    * Добавит no-js для main-nav
    
    * Добавит JS Map
    
    * Рефакторинг contacts на страницах
    
    * Рефакторинг toggle-button
    
    * Добавит доступность на toggle-button
    
    * Добавит анимацию
    
    * Рефакторинг page-header
    
    * Рефакторинг toggle-button на страницах
    
    * рефакторинг site-nav
    
    * Рефакторинг наложения секций
    
    * fix lint errors
    
    * Добавит оптимизацию
    
    * Удалит мусор
    
    * Добавит ally
    
    * Рефакторинг diet-card
    
    * Удалит мусор
    
    * Изменит название комопнента
    
    * Рефакторинг

---
+++ source/scripts/map.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const mapContainer = document.querySelector('.contacts__map-container');
+  const imageContainer = document.querySelector('.contacts__image-container');
+  // const isAvailableJS = document.documentElement.classList.contains('page--no-js');
+
+  if (!mapContainer) {
+    return;
+  }
+
+  // if (isAvailableJS) {
+  //   return;
+  // }
+
+  const ymaps = window.ymaps;
+
+  ymaps.ready(() => {
+    const yaMapContainer = document.createElement('div');
+    yaMapContainer.classList.add('contacts__ya-map');
+    yaMapContainer.id = 'map';
+
+    imageContainer.replaceWith(yaMapContainer);
+
+    const myMap = new ymaps.Map('map', {
+        center: [59.938679, 30.3230044],
+        zoom: 14,
+        controls: []
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../icons/stack.svg#icon-pin',
+        iconImageSize: [57, 53],
+        iconImageOffset: [-26, -42]
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+
+    const yaLink = document.querySelector('.ymaps-2-1-79-copyright__logo');
+    yaLink.ariaLabel = 'Янедкс.Карты';
+  });
+});
